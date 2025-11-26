f2b487ffc002c4ccbeae89ab6b888382a075351f
RAGINI BHAYANA <55350452+rrb-ragini@users.noreply.github.com>
2025-11-26 19:39:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0070f3",
+        accent: "#6b46c1"
+      }
+    }
+  },
+  plugins: []
+};
