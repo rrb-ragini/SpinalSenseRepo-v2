66ec6d1600d2279e389d625f197744e3982f8be5
RAGINI BHAYANA <55350452+rrb-ragini@users.noreply.github.com>
2025-11-27 18:02:31 +0530

    Update page.jsx

--- app/app/home/page.jsx
+++ app/app/home/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-import UploadPanel from "@components/UploadPanel";
-import ChatPanel from "@components/ChatPanel";
+import UploadPanel from "../../components/UploadPanel";
+import ChatPanel from "../../components/ChatPanel";
 
 import { useState } from "react";
 
